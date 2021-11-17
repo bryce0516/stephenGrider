01507c7c2342b00a6eac2a1f5bae9b78bfeb60aa
Hyesung Park <jayhspark@gmail.com>
2021-11-17 16:15:28 +0900

    update

--- map/src/index.ts
+++ map/src/index.ts
@@ -1,7 +1,10 @@
 import { User } from "./User";
 import { Company } from "./Compant";
+import { CustomMap } from "./CustomMap";
 console.log("hi there");
 
 const user = new User();
 const company = new Company();
+
+const customMap = new CustomMap();
 console.log(user, company);
