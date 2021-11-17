6c2530d1234e5f185ad0d6ec6b17b6a8a7da315c
Hyesung Park <jayhspark@gmail.com>
2021-11-17 15:03:48 +0900

    update

---
+++ map/src/index.ts
@@ -0,0 +1,7 @@
+import { User } from "./User";
+import { Company } from "./Compant";
+console.log("hi there");
+
+const user = new User();
+const company = new Company();
+console.log(user, company);
